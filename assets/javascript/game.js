a072de763b89b89a960f897d3c2f84f7ded5f2cd
Sarah Stevens <sssharkie1>
2016-12-05 14:41:36 -0500

    second

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,24 @@
 
 
     $(document).ready(function() {
-      console.log("js is working, you are just doing something wrong");
 
       // global variables
 
 var randomNumber = 0;
-var redGem = 0;
-var	blueGem = 0;
-var	pinkGem = 0;
-var greenGem = 0;
+var jars = 0;
+var	books = 0;
+var	chairs = 0;
+var skillet = 0;
 
 
 var wins = 0;
 var losses = 0;
 var totalScore = 0;  
 
-//console.log(gems.red * randomNumber);
-//console.log(wins+2 , losses + 2, totalScore +2);
-
 
 //functions 
 
-function compRandomNumber () {  //function generates a random number and puts in the div ID computer number
+function compRandomNumber() {  //function generates a random number 
 
 randomNumber= Math.floor((Math.random() * 120) + 12);
 var compRandomNumber = "<h3>"+ randomNumber +"</h3>";
@@ -32,45 +28,44 @@ console.log( "random comp number " + randomNumber);
 };
 
 function gemsNumber() {
-	var gemsNumberRed = Math.floor((Math.random() * 12) + 1);
-	var gemsNumberBlue = Math.floor((Math.random() * 12) + 1);
-	var gemsNumberPink = Math.floor((Math.random() * 12) + 1);
-	var gemsNumberGreen = Math.floor((Math.random() * 12) + 1);
-	redGem=gemsNumberRed;
-	blueGem=gemsNumberBlue;
-	pinkGem=gemsNumberPink;
-	greenGem=gemsNumberGreen;
-	console.log("red gem " + gemsNumberRed);
-	console.log("blue gem " + gemsNumberBlue);
-	console.log("pink gem " + gemsNumberPink);
-	console.log("green gem " + gemsNumberGreen);
-
+	var numberJars = Math.floor((Math.random() * 12) + 1);
+	var numberBooks = Math.floor((Math.random() * 12) + 1);
+	var numberChairs = Math.floor((Math.random() * 12) + 1);
+	var numberSkillet = Math.floor((Math.random() * 12) + 1);
+	jars=numberJars;
+	books=numberBooks;
+	chairs=numberChairs;
+	skillet=numberSkillet;
+	console.log("jars " + numberJars);
+	console.log("books " + numberBooks);
+	console.log("chairs " + numberChairs);
+	console.log("skillet " + numberSkillet);
 };
 
-function gemClick (){  //function adds the click choices togetther and diplays on html
-		$("#red").click(function(){
-			totalScore = redGem+totalScore;
-			$("#currentTotal").html(totalScore);
+function gemClick (){  //function totals the click choices and diplays on html
+		$("#jars").click(function(){
+			totalScore = jars+totalScore;
+			$("#runningTotal").html(totalScore);
 			console.log("ttl score" +totalScore);
 			winsLosses();
 
 		});
 
-		$("#blue").click(function(){
-			totalScore = blueGem+totalScore;
-			$("#currentTotal").html(totalScore);
+		$("#books").click(function(){
+			totalScore = books+totalScore;
+			$("#runningTotal").html(totalScore);
 			console.log("ttl score" + totalScore);
 			winsLosses();
 		});
-		$("#pink").click(function(){
-			totalScore = pinkGem+totalScore;
-			$("#currentTotal").html(totalScore);
+		$("#chairs").click(function(){
+			totalScore = chairs+totalScore;
+			$("#runningTotal").html(totalScore);
 			console.log("ttl score" +totalScore);
 			winsLosses();
 		});
-		$("#green").click(function(){
-			totalScore = greenGem+totalScore;
-			$("#currentTotal").html(totalScore);
+		$("#skillet").click(function(){
+			totalScore = skillet+totalScore;
+			$("#runningTotal").html(totalScore);
 			console.log("ttl score" + totalScore);
 			winsLosses();
 		});				
@@ -78,17 +73,17 @@ function gemClick (){  //function adds the click choices togetther and diplays o
 
 };
 
-function winsLosses(){ //counts wins and losses
+function winsLosses(){ //tracks wins and losses
 	 if (totalScore === randomNumber){
 		alert("You Win!");
 		wins++;
 		console.log("wins " + wins);
-		var winCount = "<p>Wins&nbsp;&nbsp;&nbsp;"+wins+"</p>" + "<p>Losses:" +losses+ "</p>";
+		var winCount = "<p>Wins:"+wins+"</p>" + "<p>Losses:" +losses+ "</p>";
 		document.querySelector("#winsLosses").innerHTML=winCount;
 		alert("If you want to play again, just click the play Again Box");
 		$("#playAgain").click(function(){
 		var newTotalScore = "<h2>0</h2>";
-		document.querySelector("#currentTotal").innerHTML=newTotalScore;
+		document.querySelector("#runningTotal").innerHTML=newTotalScore;
 		game();
 		});
 
@@ -100,14 +95,13 @@ function winsLosses(){ //counts wins and losses
 		document.querySelector("#winsLosses").innerHTML=lossCount;
 		$("#playAgain").click(function(){
 		var newTotalScore = "<h2>0</h2>";
-		document.querySelector("#currentTotal").innerHTML=newTotalScore;
+		document.querySelector("#runningTotal").innerHTML=newTotalScore;
 		game();
 		});
 	}
 
 };
 //functions for the game
-//---------------------------------------------------------------
 
 
 function game(){
@@ -115,7 +109,7 @@ function game(){
 	randomNumber = 0;
 	compRandomNumber();
 	gemsNumber();
-console.log("total score " + totalScore);
+
 
 }
 
